e80201845177dcdd95aea73333043630c69627cf
zj831007 <zj831007@163.com>
2012-12-16 21:23:15 +0800

    add helloworld project

---
+++ HelloWorld/HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by Justin on 12-12-16.
+//  Copyright (c) 2012年 jian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
